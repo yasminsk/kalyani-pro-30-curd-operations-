e0b851ebfa8dd3a8ae4ff5580b529890f0874fbc
kalyani <gandikotakalyani334@gmail.com>
2023-04-06 20:54:28 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.db.models.functions import Length
 # Create your views here.
 from app.models import *
 def display_topics(request):
@@ -9,10 +9,24 @@ def display_topics(request):
 
 def display_webpages(request):
     LOW=Webpage.objects.all()
+    LOW=Webpage.objects.filter(topic_name='Cricket')
+    #LOW=Webpage.objects.get(topic_name='Cricket')
+    LOW=Webpage.objects.exclude(topic_name='Cricket')
+    LOW=Webpage.objects.all()[:2:]
+    LOW=Webpage.objects.order_by('name')
+    LOW=Webpage.objects.order_by('-name')
+    LOW=Webpage.objects.order_by(Length('name'))
+    LOW=Webpage.objects.order_by(Length('name').desc())
+    
     d={'webpages':LOW}
     return render(request,'display_webpages.html',context=d)
 
 def display_access(request):
     LOA=AccessRecord.objects.all()
+    LOA=AccessRecord.objects.filter(date__gt='2012-07-25')
+    LOA=AccessRecord.objects.filter(date__lt='2012-07-25')
+    LOA=AccessRecord.objects.filter(date__gte='2012-07-25')
+    LOA=AccessRecord.objects.filter(date__lte='2012-07-25')
+    
     d={'access':LOA}
     return render(request,'display_access.html',context=d)
